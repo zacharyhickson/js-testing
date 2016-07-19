4efd9b36508e3b21b1bd72a7d5404541bc608fb3
Zachary Hickson <zacharyh96@gmail.com>
2016-07-19 12:12:45 +1000

    add deleteContact function

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -9,3 +9,7 @@ AddressBook.prototype.addContact = function(contact) {
 AddressBook.prototype.getContact = function(index) {
 	return this.contacts[index];
 }
+
+AddressBook.prototype.deleteContact = function(index) {
+	this.contacts.splice(index, 1);
+}
