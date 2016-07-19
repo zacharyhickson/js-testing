cce1af5cd14ebb909002f54c628bf82caa7c1564
Zachary Hickson <zacharyh96@gmail.com>
2016-07-19 12:09:11 +1000

    add getContact function

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -5,3 +5,7 @@ function AddressBook() {
 AddressBook.prototype.addContact = function(contact) {
 	this.contacts.push(contact);
 }
+
+AddressBook.prototype.getContact = function(index) {
+	return this.contacts[index];
+}
