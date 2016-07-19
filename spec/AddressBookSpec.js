33086f280f9b69a441b57c6ec3a105cf2858a668
Zachary Hickson <zacharyh96@gmail.com>
2016-07-19 12:11:37 +1000

    write deleteContact spec

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -7,4 +7,14 @@ describe('Address Book', function() {
 
 		expect(addressBook.getContact(0)).toBe(thisContact);
 	});
+
+	it('should be able to delete a contact', function() {
+		var addressBook = new AddressBook(),
+			thisContact = new Contact();
+
+		addressBook.addContact(thisContact);
+		addressBook.deleteContact(0);
+
+		expect(addressBook.getContact(0)).not.toBeDefined();
+	})
 });
