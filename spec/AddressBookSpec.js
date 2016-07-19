9d41cec6e868b4374305725b4c377ca5c75298df
Zachary Hickson <zacharyh96@gmail.com>
2016-07-19 12:15:19 +1000

    remove redundant code

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,17 +1,19 @@
 describe('Address Book', function() {
-	it('should be able to add a contact', function() {
-		var addressBook = new AddressBook(),
-			thisContact = new Contact();
+	var addressBook,
+		thisContact;
 
+	beforeEach(function() {
+		addressBook = new AddressBook();
+		thisContact = new Contact();
+	});
+
+	it('should be able to add a contact', function() {
 		addressBook.addContact(thisContact);
 
 		expect(addressBook.getContact(0)).toBe(thisContact);
 	});
 
 	it('should be able to delete a contact', function() {
-		var addressBook = new AddressBook(),
-			thisContact = new Contact();
-
 		addressBook.addContact(thisContact);
 		addressBook.deleteContact(0);
 
